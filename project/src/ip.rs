d29b1c1e0c6d2b4b5d2cd6e85647309dac948178
Vilquid <60876471+Vilquid@users.noreply.github.com>
2024-06-08 18:57:17 +0200

    Improved setup script

---
+++ project/src/ip.rs
@@ -0,0 +1,132 @@
+use std::{fs, io};
+use std::fs::OpenOptions;
+use std::io::{BufRead, BufReader};
+use std::net::{SocketAddr, TcpStream};
+use std::time::Duration;
+use regex::Regex;
+use dns_lookup::lookup_addr;
+use dotenv::dotenv;
+use std::io::Write;
+use crate::bdd::add_domain;
+use crate::logger::log;
+
+
+/// # Brief
+/// Check if the IP is valid and if the port 25 is open
+/// # Attributes
+/// inline
+/// # Parameters
+/// - `ip` *String* - IP to check
+/// # Return
+/// **Nothing**
+/// # Usage
+/// mod ip;
+/// ip("8.8.8.8".to_string());
+#[inline]
+pub(crate) fn ip(ip: String)
+{
+	// Verify if the IP is valid
+	if !Regex::new(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(:\d{1,5})?$").unwrap().is_match(&ip)
+	{
+		log("ERROR", format!("The IP {} is not valid", ip).as_str());
+		return;
+	}
+
+	let socket = format!("{}:{}", ip, "25").parse::<SocketAddr>().unwrap();
+
+	// if the port 25 is not open
+	if TcpStream::connect_timeout(&socket, Duration::from_millis(200)).is_err()
+	{
+		return;
+	}
+
+	// if the port 25 is open
+	let ip: std::net::IpAddr = ip.parse().unwrap();
+	let host = lookup_addr(&ip).unwrap();
+	let host = host.to_string();
+	
+	log("INFO", format!("The IP {} is valid -> Starting to get the DNS record of {}", ip, host).as_str());
+	// add_domain(&host);
+}
+
+/// # Brief
+/// Get the last scanned IP
+/// # Attributes
+/// inline
+/// # Return
+/// **String**
+/// # Usage
+/// mod ip;
+/// let last_ip = last_scanned_ip();
+#[inline]
+pub fn last_scanned_ip() -> String
+{
+	dotenv().ok();
+	std::env::var("LAST_SCANNED_IP").unwrap().to_string()
+}
+
+/// # Brief
+/// Save the last scanned IP in the environment variables
+/// # Attributes
+/// inline
+/// # Parameters
+/// - `ip` *String* - IP to save
+/// # Return
+/// **Nothing**
+/// # Usage
+/// mod ip;
+/// save_last_scanned_ip("1.1.1.1".to_string());
+#[inline]
+pub fn save_last_scanned_ip(ip: String) -> io::Result<()>
+{
+	let dotenv_path = ".env";
+	let temp_path = ".env.tmp";
+
+	// Tenter d'ouvrir le fichier .env, gérer l'erreur s'il n'existe pas
+	let input = match fs::File::open(dotenv_path)
+	{
+		Ok(file) => file,
+		Err(e) => {
+			eprintln!("Erreur lors de l'ouverture de {}: {}", dotenv_path, e);
+			return Err(e);
+		}
+	};
+	let buffered = BufReader::new(input);
+
+	let mut output = match OpenOptions::new()
+		.write(true)
+		.create(true)
+		.truncate(true)
+		.open(temp_path)
+	{
+		Ok(file) => file,
+		Err(e) => {
+			return Err(e);
+		}
+	};
+
+	for line in buffered.lines()
+	{
+		let line = match line {
+			Ok(ln) => ln,
+			Err(e) => {
+				return Err(e);
+			}
+		};
+		
+		if line.starts_with("LAST_SCANNED_IP=")
+		{
+			if let Err(e) = writeln!(output, "LAST_SCANNED_IP={}", ip)
+			{
+				return Err(e);
+			}
+		} else if let Err(e) = writeln!(output, "{}", line) {
+  			return Err(e);
+		}
+	}
+
+	// Renommer le fichier temporaire pour remplacer l'ancien .env
+	fs::rename(temp_path, dotenv_path)?;
+
+	Ok(())
+}
--- project/src/main.rs
+++ project/src/main.rs
@@ -1,16 +1,128 @@
+use std::env;
+use std::sync::mpsc::channel;
+use std::thread;
+use dotenv::dotenv;
+
+use logger::log;
+use crate::ip::save_last_scanned_ip;
+
 pub mod logger;
 pub mod schema;
 pub mod bdd;
 pub mod models;
 mod domain;
 mod get_cert;
-
-
-// use logger::log;
-// use crate::bdd::add_domain;
+mod ip;
 
 
 fn main()
 {
-	println!("Hi comrade glacier");
+	log("INFO", "Start of Comrade Glacier");
+
+	//in this part I get the number of threads an create them with a channel to transmit informations between the main thread and the worker threads
+	// if the number of threads is not specified, the default value is 6
+	let number_of_threads = env::var("NUM_THREADS").unwrap_or("10".to_string()).parse::<usize>().unwrap();
+
+	// Creation of an array to make it easier to iterate over the possibilities of ip addresses
+	let mut ip: [u32; 4] = [0, 0, 0, 0];
+
+	if dotenv().ok().is_some()
+	{
+		let last_ip = ip::last_scanned_ip();
+		let last_ip: Vec<&str> = last_ip.split('.').collect();
+		ip[0] = last_ip[0].parse::<u32>().unwrap();
+		ip[1] = last_ip[1].parse::<u32>().unwrap();
+		ip[2] = last_ip[2].parse::<u32>().unwrap();
+		ip[3] = last_ip[3].parse::<u32>().unwrap();
+	}
+
+	// Creation of a channel to communicate from the workers to the main thread, i need just one because channels allows multiple senders and i have only one receiver
+	let (mainspeaker, _mainlistener) = channel();
+
+	//i create a vector to store the senders to the workers and i specify that only Strings will be passed to these senders
+	let mut annuaire: Vec<std::sync::mpsc::Sender<String>> = Vec::new();
+	let mut worker_list: Vec<thread::JoinHandle<()>> = Vec::new();
+
+	// i iterate to create number_of_threads - 1 workers. i create their channels and store theirs senders in the annuaire vector. i also clone the main speaker and pass it to the worker to get the results of its computation
+	for _i in 0..number_of_threads
+	{
+		let (speaker, listener) = channel();
+		annuaire.push(speaker);
+		let speaker2 = mainspeaker.clone();
+		worker_list.push(thread::spawn(move || {
+			let mut message = listener.recv().unwrap();
+
+			#[allow(unused_variables)]
+				let mut numero = 0;
+
+			while message != "extinction"
+			{
+				numero += 1;
+				let phrase = message.to_string();
+				// println!("Worker {}", phrase);
+				ip::ip(message.clone());
+
+				let _ = save_last_scanned_ip(message.to_string());
+
+				speaker2.send(phrase).unwrap();
+				message = listener.recv().unwrap();
+			}
+		}));
+	}
+
+	// i create a flag to stop the iteration over ip adresses when the last possibility is reached
+	let mut flag = true;
+
+	//i iterate over the possibilities of ip addresses and pass them to the workers
+	while flag
+	{
+		#[allow(clippy::needless_range_loop)]
+			for i in 0..number_of_threads
+			{
+				let message = format!("{}.{}.{}.{}", ip[0], ip[1], ip[2], ip[3]);
+				annuaire[i].send(message).unwrap();
+
+				ip[3] += 1;
+				if ip[3] == 254
+				{
+					ip[3] = 1;
+					ip[2] += 1;
+				}
+				if ip[2] == 255
+				{
+					println!("Current IP : {}.{}.{}.{}", ip[0], ip[1], ip[2], ip[3]);
+					ip[2] = 0;
+					ip[1] += 1;
+				}
+				if ip[1] == 255
+				{
+					// println!("Current IP : {}.{}.{}.{}", ip[0], ip[1], ip[2], ip[3]);
+					ip[1] = 0;
+					ip[0] += 1;
+				}
+				if ip[0] == 256
+				{
+					// println!("Current IP : {}.{}.{}.{}", ip[0], ip[1], ip[2], ip[3]);
+					flag = false;
+					break;
+				}
+			}
+	}
+
+	// i send the extinction signal to the workers so they will find at the end the stop message
+	for i in annuaire.iter()
+	{
+		i.send("extinction".to_string()).unwrap();
+	}
+
+	// when all the data are sent to the workers i wait for the them to finish their job
+	for i in worker_list.into_iter()
+	{
+		i.join().unwrap();
+	}
+	// I drop the main speaker to hang up the channel and avoid being stuck in the last loop
+	drop(mainspeaker);
+
+	log("INFO", "End of Comrade Glacier");
 }
+
